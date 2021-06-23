4338f0aca67fe4a3e18ab91500176d2e09b61901
cweijan <cweijan@163.com>
2021-06-23 14:27:29 +0800

    Remove context menu.

--- browser/src/containers/App/ContextMenu.ts
+++ browser/src/containers/App/ContextMenu.ts
@@ -17,6 +17,7 @@ function adjustPosition(menu: HTMLElement, event: MouseEvent): void {
 
 export class ContextMenu {
     public static create(event: MouseEvent): void {
+        event.stopPropagation()
         let menu = document.getElementById('menu');
         if (menu != null) {
             adjustPosition(menu, event);
